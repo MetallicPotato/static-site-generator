502c0fab7dfea290105b574192cd012b1f5624a8
MetallicPotato <primemeats@gmail.com>
2026-01-07 18:44:48 -0600

    added more tests for textnode conversions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -29,6 +29,38 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        node = TextNode("this is text!", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "this is text!")
+    
+    def test_italic(self):
+        node = TextNode("this is text!", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "this is text!")
+
+    def test_code(self):
+        node = TextNode("this is text!", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "this is text!")
+    
+    def test_link(self):
+        node = TextNode("this is text!", TextType.LINK, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "this is text!")
+        self.assertEqual(html_node.props["href"], "https://www.google.com")
+    
+    def test_image(self):
+        node = TextNode("this is text!", TextType.IMAGE, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.props["alt"], "this is text!")
+        self.assertEqual(html_node.props["src"], "https://www.google.com")
 
 if __name__ == "__main__":
     unittest.main()
