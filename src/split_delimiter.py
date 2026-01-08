44bf562e453ee07d335cb03496279a3e63e5c5a1
pickles <primemeats@gmail.com>
2026-01-07 22:22:45 -0600

    started split delimiter

---
+++ src/split_delimiter.py
@@ -0,0 +1,9 @@
+from textnode import TextType
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if not old_nodes:
+        raise ValueError("Cannot split empty list of nodes")
+    for node in old_nodes:
+        pass
+    #TODO: finish implementing function!
