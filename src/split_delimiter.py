044b78c50744ba6ca38a4c65ec4757d42764f3f6
pickles <primemeats@gmail.com>
2026-01-10 19:22:00 -0600

    implemented split_delimiter

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -1,9 +1,19 @@
-from textnode import TextType
+from textnode import TextType, TextNode
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     if not old_nodes:
         raise ValueError("Cannot split empty list of nodes")
+    new_nodes = []
     for node in old_nodes:
-        pass
-    #TODO: finish implementing function!
+        if node.text_type == text_type:
+            split_text = node.text.split(delimiter)
+            inside_split = False
+            for t in split_text:
+                if inside_split:
+                    new_nodes.append(TextNode(t, text_type))
+                else:
+                    new_nodes.append(TextNode(t, TextType.TEXT))
+        else:
+            new_nodes.append(node)
+    return new_nodes
