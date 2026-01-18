d7180bfe065644de0dfcda3702bc252140b6165f
pickles <primemeats@gmail.com>
2026-01-18 16:50:02 -0600

    more work on html nodes

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -1,8 +1,37 @@
+from htmlnode import LeafNode, ParentNode
 from markdown_to_blocks import markdown_to_blocks
 from blocks import block_to_block_type, BlockType
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
+    html_blocks = []
     for block in blocks:
         block_type = block_to_block_type(block)
-        
+        new_node = None
+        match block_type:
+            case BlockType.HEADING:
+                new_node = LeafNode("h1", block)
+            case BlockType.QUOTE:
+                new_node = LeafNode("blockquote", block)
+            case BlockType.CODE:
+                code = LeafNode("code", block)
+                new_node = ParentNode("pre", [code])
+            case BlockType.ORDERED_LIST:
+                new_node = _list_iterate(block, True)
+            case BlockType.UNORDERED_LIST:
+                new_node = _list_iterate(block, False)
+            case BlockType.PARAGRAPH:
+                new_node = LeafNode("p", block)
+            case _:
+                raise ValueError(f"invalid block type: {block_type}")
+        html_blocks.append(new_node)
+    #TODO: more stuff!
+
+
+
+def _list_iterate(block, ordered: bool):
+    ordered_string = "ol" if ordered else "ul"
+    parent_node = ParentNode(ordered_string, [])
+    for line in block.splitlines():
+        parent_node.children.append(LeafNode("li", line))
+    return parent_node
