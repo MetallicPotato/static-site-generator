9a364fea56aa9c5cda49c52abb8425d0cd979542
MetallicPotato <primemeats@gmail.com>
2026-01-22 22:07:46 -0600

    work on copying files

--- src/main.py
+++ src/main.py
@@ -3,15 +3,22 @@ import shutil
 
 
 def main():
-    copy_contents()
+    print(os.listdir())
 
 def copy_contents():
     public_path = os.path.abspath("public/")
-    src_path = os.path.abspath("src/")
+    static_path = os.path.abspath("static/")
     if os.path.exists("public/"):
         print(public_path)
         shutil.rmtree(public_path)
-    #TODO: finish!
+    os.mkdir(public_path)
+    dir_contents = os.listdir(static_path)
+    
+    for file_name in dir_contents:
+
+        copy_dir = f"{static_path}{file_name}"
+        dest_dir = f"{public_path}{file_name}"
+        shutil.copy(copy_dir, dest_dir)
 
 
 if __name__ == "__main__":
