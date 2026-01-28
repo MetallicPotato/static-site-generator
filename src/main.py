629f5467bdfe9c54e9ae26fc6ff76a124ef211bd
pickles <primemeats@gmail.com>
2026-01-27 21:52:27 -0600

    added recursive page generation

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 from markdown_to_html_node import markdown_to_html_node
-from htmlnode import HTMLNode
+from pathlib import Path
 from extract_title import extract_title
 
 
 def main():
     static_to_public()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content/", "template.html", "public/")
 
 def static_to_public():
     public_path = os.path.abspath("public/")
@@ -19,18 +19,17 @@ def static_to_public():
     os.mkdir(public_path)
     copy_contents(static_path, public_path)
 
-def copy_contents(filepath, destinationpath):
+def copy_contents(filepath, destination):
     print(f"copy static: {filepath}")
     dir_contents = os.listdir(filepath)
     for file_name in dir_contents:
         copy_dir = f"{filepath}/{file_name}"
         if os.path.isfile(copy_dir):
-            dest_dir = f"{destinationpath}/{file_name}"
-            #TODO: this dest_dir needs to respect the file locations from the copy_dir!
+            dest_dir = f"{destination}/{file_name}"
             shutil.copy(copy_dir, dest_dir)
             print(f"Copied file {copy_dir}")
         else:
-            new_destination = f"{destinationpath}/{file_name}"
+            new_destination = f"{destination}/{file_name}"
             os.mkdir(new_destination)
             copy_contents(copy_dir, new_destination)
 
@@ -41,7 +40,6 @@ def generate_page(from_path, template_path, dest_path):
     markdown_text = markdown_file.read()
     markdown_html = markdown_to_html_node(markdown_text)
     html_text = markdown_html.to_html()
-    #TODO: fix this to_html call! getting ValueErrors about leaf nodes!
     page_title = extract_title(markdown_text)
     template_text = template_file.read()
     template_text = template_text.replace("{{ Title }}", page_title)
@@ -52,12 +50,21 @@ def generate_page(from_path, template_path, dest_path):
         new_file = open(dest_path, "w")
         new_file.write(template_text)
     else:
-        newpath = os.path.dirname(dest_path)
-        if os.path.exists(newpath) == False:
-            os.makedirs(newpath)
+        new_path = os.path.dirname(dest_path)
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
         new_file = open(dest_path, "w")
         new_file.write(template_text)
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
     
 
 
